cc97a00d089a2d8b1bc8ca80f7294b519a115fa3
Bugale, Vivek <vivek_bugale@bmc.com>
2025-02-12 12:29:34 +0530

    Added set monitor

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const utils = require("@bmc-compuware/ispw-action-utilities");
 
+let setID;
+let setUrl;
+
 try {
   // Inputs received from workflow
   let inputs = [
@@ -103,6 +106,17 @@ try {
     .then(
       (response) => {
         console.log("The promote request submitted successfully.");
+        let skipWaitingForSetCompletion = false;
+        if (!skipWaitingForSetCompletion) {
+          if (setID) {
+            utils.pollSetStatus(setUrl, setID, inputs.ces_token, "Deploy");
+          }
+        }
+        if (skipWaitingForSetCompletion) {
+          console.log(
+            "Skip waiting for the completion of the set for this job..."
+          );
+        }
       },
       (error) => {
         core.debug(error.stack);
@@ -204,6 +218,16 @@ function setOutputs(core, responseBody) {
       utils.stringHasContent(responseBody.message) &&
       responseBody.message.includes("timed out");
     core.setOutput("is_timed_out", isTimedOut);
+
+    if (responseBody.setId) {
+      console.log("Code Pipeline: Set Id - ", responseBody.setId);
+      setID = responseBody.setId;
+    }
+
+    if (responseBody.url) {
+      console.log("Code Pipeline: Set Info Url - ", responseBody.url);
+      setUrl = responseBody.url;
+    }
   }
 }
 
